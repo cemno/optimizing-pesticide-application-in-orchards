92919ef0201965ec305fffc1be193c3d50b51e0b
Adrian Fülle <s7adfuel@uni-bonn.de>
2021-06-22 20:53:14 +0200

    edits in the model test run .R

--- model_test_runs.R
+++ model_test_runs.R
@@ -85,6 +85,125 @@
 library(readxl)
 library(decisionSupport)
 
+gompertz_yield(
+  max_harvest = 0.8,
+  time_to_first_yield_estimate = 3,
+  time_to_second_yield_estimate = 10,
+  first_yield_estimate_percent = 20,
+  second_yield_estimate_percent = 100,
+  n_years = 10,
+  var_CV = 0,
+  no_yield_before_first_estimate = TRUE
+)
+
+vv(
+  var_mean,
+  var_CV,
+  n,
+  distribution = "normal",
+  absolute_trend = NA,
+  relative_trend = NA,
+  lower_limit = NA,
+  upper_limit = NA
+)
+
+
+vv(
+  var_mean = biodiversity,
+  var_CV = var_CV,
+  n = n_years,
+  distribution = "normal",
+  absolute_trend = bio_change,
+  lower_limit = 0.4,
+  upper_limit = 0.6
+)
+
+bio_change = biodiversity * pesticides
+  
+chance_event(
+  chance,
+  value_if = 1,
+  value_if_not = 0,
+  n = 1,
+  CV_if = 0,
+  CV_if_not = CV_if,
+  one_draw = FALSE
+)
+
+
+
+
+ascs_estimates <- read_excel("data/input_data_02.xlsx")
+
+ascs_decision_model <- function(){
+
+  # machine costs are applied only once so n = 1
+machine_cost <- rep( c(initial_cost, rep(0, 9)) )
+  
+  # vv(var_mean = initital_cost,
+  # var_CV = var_CV,
+  # n = 1)    
+
+  # costs for pesticides can vary over the years
+pesticide_cost <- vv(
+  var_mean = pesticide_costs,
+  var_CV = var_CV,
+  n = n_years)
+  # the amount of used pesticides can vary depending on weather and infestation/disease levels
+pesticide_quantity <- vv(
+  var_mean = pesticide_application_quantity,
+  var_CV = var_CV,
+  n = n_years)
+  # application efficiency (tree distance) the efficiency is fixed per model run (n=1)
+pesticide_app_reduction <- vv(
+  var_mean = reduction,
+  var_CV = var_Cv,
+  n = 1)
+
+  #  --- fixed values for both decisions and one run
+  # machine costs will be depreciated over the time the machine is in use
+fixed_machine_cost_yearly = 0 - machine_cost/n_years
+  # pesticide 
+fixed_pesticide_cost_yearly = pesticide_cost * pesticide_quantity
+
+
+
+  #  --- values for intervention
+  # pesticide yearly cost, if automation of machine is applied (no intervention)
+pesticide_cy_int = pesticide_cost_yearly * pesticide_app_reduction
+
+
+
+  #  --- values if no intervention
+pesticide_cy_int = 
+
+  
+  
+  #  --- NPV   
+farmers_profit_int = fruit_sales - pesticide_cost_yearly - machine_cost_yearly
+farmers_profit_no_int = fruit_sales - pesticide_cost_yearly
+
+return(list(Interv_NPV = farmers_profit_int,
+            NO_Interv_NPV = farmers_profit_no_int,
+            NPV_decision_do = farmers_profit_int - farmers_profit_no_int,))
+}
+
+
+ascs_estimates[ascs_estimates$variable=="yield","upper"]
+
+vv(
+  var_mean = biodiversity,
+  var_CV = var_CV,
+  n = n_years,
+  distribution = "normal",
+  absolute_trend = bio_change,
+  lower_limit = 0.4,
+  upper_limit = 0.6
+)
+
+
+
+
 
 ascs_estimates <- read_excel("data/input_data_02.xlsx")
 
